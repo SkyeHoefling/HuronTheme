cb0d4d2fe093e74816d894c0bec7353053a5552c
Andrew Hoefling <andrew@hoefling.me>
2020-08-09 15:09:10 -0400

    Implemented copying code snippets to clipboard

--- src/js/custom.js
+++ src/js/custom.js
@@ -11,6 +11,12 @@ $('.title-bar').on('sticky.zf.stuckto:top', function(){
 $(document).ready(function() {
     $('.code-snippet').append('<span class="code-snippet-copy">Copy</span>');
     $('.code-snippet-copy').click(function (e) {
-        alert('copy');
-    })
+        navigator.clipboard.writeText($(e.target).prev()[0].innerText)
+            .then(function() {
+                e.target.innerText = "Copied!";
+            }, function (error) {
+                alert('error, check console');
+                console.log(error);
+            });
+    });
 });
