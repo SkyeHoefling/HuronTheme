704bf9f29f6b88ecd1f8d0a8377b11dffda29a4e
Andrew Hoefling <andrew@hoefling.me>
2020-08-09 14:50:20 -0400

    added event handler and dom elements for copying code snippets

--- src/js/custom.js
+++ src/js/custom.js
@@ -7,3 +7,10 @@ $('.title-bar').on('sticky.zf.stuckto:top', function(){
 }).on('sticky.zf.unstuckfrom:top', function(){
     $(this).css('display', 'none');
 }).on();
+
+$(document).ready(function() {
+    $('.code-snippet').append('<span class="code-snippet-copy">Copy</span>');
+    $('.code-snippet-copy').click(function (e) {
+        alert('copy');
+    })
+});
